9fe0400445da48649c2b183d4a729a48f1498e8d
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-02-22 15:19:18 +0100

    add movie card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { AddMovie } from "./components/AddMovie";
+import { MovieCard } from "./components/MovieCard";
 export function App() {
   return (
     <>
       <AddMovie />
+      <MovieCard />
     </>
   );
 }
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,8 +1,12 @@
-
-
-export function MovieCard(){
-    return <div className="card">
-
+export function MovieCard() {
+  return (
+    <div className="card">
+      <h4>Title</h4>
+      <div className="info">
+        <p>num /5</p>
+        Genre
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias sunt neque facere architecto eius nam rerum doloribus nihil distinctio officiis qui provident iste maiores, autem dolores recusandae vel asperiores ratione.</p>
     </div>
-
+  );
 }
